4919c668f2e9053ffff93a8cfcd6cb53d3bf66fc
jorge-dominguez <jdombusiness@gmail.com>
2018-10-19 15:35:50 -0300

    Initial commit

---
+++ src/main/java/com/example/circleci/CircleciDemoJavaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.circleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciDemoJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleciDemoJavaSpringApplication.class, args);
+    }
+}
